65c7cae57dc15e1cdee1a2883126d7fce67516d1
raj tripathi <tripathiraj3030@gmail.com>
2025-03-01 12:42:35 +0530

    Switched to opencv-python-headless to fix libGL error

--- Home.py
+++ Home.py
@@ -2,12 +2,13 @@ import streamlit as st
 
 basics_page = st.Page("Basics.py", title = "Basics of Computer Vision", icon = ":material/book:")
 cd_page = st.Page("Color_Detection.py", title = "Color Detection", icon = ":material/palette:")
+morph_page = st.Page("Morphological_Operations.py", title = "Morphological Operations", icon = ":material/transform:")
 mp1_page = st.Page("Mini Projects/doc_scanner.py", title = "Document Scanner", icon = ":material/docs:")
 # tester_page = st.Page("tester.py", title = "Tester", icon = ":material/tactic:")
 
 pg = st.navigation(
     {
-        "Learning" : [basics_page, cd_page],
+        "Learning" : [basics_page, cd_page, morph_page],
         "Mini Projects" : [mp1_page]
     })
 st.set_page_config(page_title="Computer Vision App", page_icon=":material/radio_button_unchecked:")
